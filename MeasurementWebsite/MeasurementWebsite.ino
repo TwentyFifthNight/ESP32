1234b0979f165a2c050ab6eb8e0b8f889f10f08f
TwentyFifthNight <damian25th@gmail.com>
2024-06-05 19:56:43 +0200

    control x labels count and labels size change
    Added a variable to set the number of x labels and used pixels for the label line size instead of a percentage.

--- MeasurementWebsite/MeasurementWebsite.ino
+++ MeasurementWebsite/MeasurementWebsite.ino
@@ -9,18 +9,22 @@
 #include "time.h"
 
 uint16_t dataSize = 0;
-const uint16_t dataTargetSize = 300;
+const uint16_t dataTargetSize = 101;
 uint16_t startIndex = 0;
 float dataT[dataTargetSize] = {0.0};
 float dataP[dataTargetSize] = {0.0};
 float dataH[dataTargetSize] = {0.0};
-const int lineMargin = 5; //distance of the line from the vertical edges of the chart(percents)
 
 WebServer webServer(80);
 String sOut="";
 const uint16_t autorefresh = 2000;
+
 String chartWidth = "80";
 String chartHeight = "10";
+const int lineMargin = 5; //distance of the line from the vertical edges of the chart(percents)
+const int chartXOffset = 0;
+const byte xLabelCount = 21;
+const byte xSecondLabelCount = 5;
 
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 3600;
@@ -123,7 +127,7 @@ void handleRoot() {
   .main_content{text-align:center;}\
   .axismark-main {stroke: black;stroke-width: 1;}\
   .axismark-second {stroke: black;stroke-width: 1;}\
-  .axisnumber-x {font-size: 12px;}\
+  .axisnumber-x {font-size: 12px;color: black;}\
   .axisnumber-y {font-size: 12px;color: black;}\
   </style> </head><body>\
   <h1 class=\"font_t\">ESP32 laboratorium</h1>";
@@ -148,7 +152,7 @@ void handleRoot() {
   webServer.sendContent(sOut);
   PrintWykres(dataSize, 3, dataH);
   
-  sOut="</div></div>";
+  sOut="</div></div><br>";
 
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
@@ -178,15 +182,17 @@ void handleNotFound(){
 void PrintWykres(int dataLen, int color, float* chartData) {
   char tempS[150];
   String output = "";
-  output += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\"width=\""+ chartWidth + "vw\" height=\"" + chartHeight + "vh\" overflow=visible>\n";
-  output += "<rect width=\"" + chartWidth + "vw\" height=\"" + chartHeight + "vh\" fill=\"rgb(200, 200, 200)\" stroke-width=\"1\" stroke=\"rgb(255, 255, 255)\" />\n";
+  output += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\"width=\""+ chartWidth + "dvw\" height=\"" + chartHeight + "dvh\" overflow=visible>\n";
+  sprintf(tempS, "<g transform=\"translate(%d, 0)\">", chartXOffset);
+  output += tempS;
+  output += "<rect width=\"100%\" height=\"100%\" fill=\"rgb(200, 200, 200)\" stroke-width=\"1\" stroke=\"rgb(255, 255, 255)\" />\n";
   float miny=1000;
   float maxy=-1000;
   float dx, scale;
   float y, newY;
   float x, newX;
   unsigned int maxElements = 400;
-  float xShift = 100 * 1. / dataTargetSize;
+  float xShift = 100 * 1. / (dataTargetSize - 1);
 
   dataLen = (dataTargetSize > dataLen ? dataLen : dataTargetSize);
   for (int i=0; i< dataLen ; i++) {
@@ -200,27 +206,47 @@ void PrintWykres(int dataLen, int color, float* chartData) {
   else scale = (100 - lineMargin * 2);
 
   output += "<g id=\"axis-y-values\" class=\"axisnumber-y\" text-anchor=\"end\">";
-  sprintf(tempS, "<text y=\"%d%%\" x=\"%d%%\">%.2f</text>", 0 + lineMargin, -1, maxy);
+  sprintf(tempS, "<text y=\"%d%%\" x=\"%d\">%.2f</text>", 0 + lineMargin, -8, maxy);
   output += tempS;
-  sprintf(tempS, "<text y=\"%d%%\" x=\"%d%%\">%.2f</text>", 100 - lineMargin, -1, miny);
+  sprintf(tempS, "<text y=\"%d%%\" x=\"%d\">%.2f</text>", 100 - lineMargin, -8, miny);
   output += tempS;
+
+  if(dx >= 0.02){
+    sprintf(tempS, "<text y=\"%d%%\" x=\"%d\">%.2f</text>", 50, -8, miny + dx / 2);
+    output += tempS;
+  }
+  
   output += "</g>";
 
   output += "<g id=\"axis-x-values\" class=\"axisnumber-x\" text-anchor=\"middle\">";
 
-  unsigned int step = dataTargetSize / 10;
-  for(unsigned int i = 0; i <= dataTargetSize; i += step){
-    sprintf(tempS, "<text y=\"%d%%\" x=\"%f%%\">%d</text>", 120, xShift * i, i);
+  unsigned int step = dataTargetSize / (xLabelCount - 1);
+
+  unsigned int labelStart = 0;
+  unsigned int labelEnd = dataTargetSize - 1;
+  if(dataSize > dataTargetSize){
+    labelStart = dataSize - dataTargetSize;
+    labelEnd = dataSize - 1;
+  }
+
+  for(unsigned int i = labelStart; i <= labelEnd; i += step){
+    sprintf(tempS, "<text y=\"%d%%\" x=\"%f%%\" transform=\"translate(0, 18)\">%d</text>", 100, xShift * (i - labelStart), i);
     output += tempS;
   }
   output += "</g>";
 
-  output += "<defs xmlns=\"http://www.w3.org/2000/svg\">\
-  <pattern id=\"x-axismark-main\" x=\"0\" width=\"10%\" height=\"6\" patternUnits=\"userSpaceOnUse\">\
-  <line x1=\"0\" y1=\"0\" x2=\"0\" y2=\"6\" class=\"axismark-main\"/>\
-  </pattern>\
-  <pattern id=\"x-axismark-second\" x=\"0\" width=\"2.5%\" height=\"3\" patternUnits=\"userSpaceOnUse\">\
-  <line x1=\"0\" y1=\"-1\" x2=\"0\" y2=\"3\" class=\"axismark-second\"/>\
+  output += "<defs xmlns=\"http://www.w3.org/2000/svg\">";
+  
+  sprintf(tempS, "<pattern id=\"x-axismark-main\" x=\"0\" width=\"%d%%\" height=\"6\" patternUnits=\"userSpaceOnUse\">", 100 / (xLabelCount - 1));
+  output += tempS;
+  
+  output += "<line x1=\"0\" y1=\"0\" x2=\"0\" y2=\"6\" class=\"axismark-main\"/>\
+  </pattern>";
+
+  sprintf(tempS, "<pattern id=\"x-axismark-second\" x=\"0\" width=\"%f%%\" height=\"3\" patternUnits=\"userSpaceOnUse\">", float(100 / (xLabelCount - 1)) / (xSecondLabelCount + 1));
+  output += tempS;
+
+  output += "<line x1=\"0\" y1=\"-1\" x2=\"0\" y2=\"3\" class=\"axismark-second\"/>\
   </pattern>";
   sprintf(tempS, "<pattern id=\"y-axismark-main\" y=\"-%d%%\" width=\"6\" height=\"%d%%\" patternUnits=\"userSpaceOnUse\">", lineMargin, 50 - lineMargin);
   output += tempS;
@@ -233,9 +259,9 @@ void PrintWykres(int dataLen, int color, float* chartData) {
   </defs>";
 
   output += "<g transform=\"scale(1, -1)\">\
-  <rect id=\"x-axismark2\" x=\"0\" y=\"-103%\" width=\"100%\" height=\"3\" fill=\"url(#x-axismark-second)\"/>\
-  <rect id=\"x-axismark\" x=\"0\" y=\"-106%\" width=\"101%\" height=\"6\" fill=\"url(#x-axismark-main)\"/>\
-  <rect id=\"y-axismark2\" x=\"-3\" y=\"-100%\" width=\"3\" height=\"100%\" fill=\"url(#y-axismark-second)\"/>\
+  <rect id=\"x-axismark2\" x=\"0\" y=\"calc(-100% - 3px)\" width=\"100.5%\" height=\"calc(100% + 3px)\" fill=\"url(#x-axismark-second)\"/>\
+  <rect id=\"x-axismark\" x=\"0\" y=\"calc(-100% - 6px)\" width=\"100.5%\" height=\"6\" fill=\"url(#x-axismark-main)\"/>\
+  <rect id=\"y-axismark2\" x=\"-3\" y=\"-100%\" width=\"calc(100% + 3px)\" height=\"100%\" fill=\"url(#y-axismark-second)\"/>\
   <rect id=\"y-axismark\" x=\"-6\" y=\"-100%\" width=\"6\" height=\"101%\" fill=\"url(#y-axismark-main)\"/>\
   </g>";
   webServer.sendContent(output);
@@ -281,6 +307,7 @@ void PrintWykres(int dataLen, int color, float* chartData) {
     x = newX;
   }
 
+  output += "</g>\n"; 
   output += "</g>\n"; 
   output += "</svg>\n";
   webServer.sendContent(output);
